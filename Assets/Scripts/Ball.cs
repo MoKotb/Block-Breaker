066f8f1cd47ff52541c0b6a02b278c10c62cce64
Mohamed Kotb <mo.kotb96@gmail.com>
2019-04-25 02:12:26 +0200

    Add Paddle And Ball To Project

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] Paddle paddle;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 12f;
+    private Vector2 paddleToBallVector;
+    private bool hasStarted = false;
+
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle.transform.position;
+    }
+
+    void Update()
+    {
+        if (!hasStarted) {
+            LockBallToPaddle();
+            LaunchBall();
+        }
+    }
+
+    private void LockBallToPaddle() {
+        Vector2 paddlePosition = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
+        transform.position = paddlePosition + paddleToBallVector;
+    }
+
+    private void LaunchBall() {
+        if (Input.GetMouseButtonDown(0)) {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+
+    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float maxX = 15f;
+    [SerializeField] float minX = 1f;
+
+    void Update()
+    {
+        float mousePosition = (Input.mousePosition.x / Screen.width) * screenWidthInUnits;
+        Vector2 paddlePosition = new Vector2(transform.position.x, transform.position.y);
+        paddlePosition.x = Mathf.Clamp(mousePosition,minX,maxX);
+        transform.position = paddlePosition;
+    }
+}
